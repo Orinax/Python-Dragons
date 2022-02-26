76303faab326b41eaae48757679c4151d3df9430
Orinax <chaunceypelton@gmail.com>
2022-02-26 18:47:29 +0700

    First commit

---
+++ first_class.py
@@ -0,0 +1,14 @@
+class Ice_Dragon:
+    def create_dragon(self, name, color, level=1):
+        self.name = name
+        self.color = color
+        self.level = level
+
+    def fight(self):
+        self.level += 1
+
+    def frost_breath(self):
+        self.mouth = 'open'
+        self.breath = 'freezing'
+        print(f'{self.name}\'s mouth is {self.mouth}'
+              f' and her breath is {self.breath}.')
