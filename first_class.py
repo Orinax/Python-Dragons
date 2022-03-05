2af6e07c0a6039e6a1e37c4c02955f88d5a55938
Orinax <chaunceypelton@gmail.com>
2022-03-05 19:13:46 +0700

    Added doc strings

--- first_class.py
+++ first_class.py
@@ -1,13 +1,18 @@
 class Ice_Dragon:
+    """A class used to build Ice Dragons."""
+
     def __init__(self, name, color, level=1):
+        """Creates an Ice Dragon at level 1."""
         self.name = name
         self.color = color
         self.level = level
 
     def fight(self):
+        """Increases the dragon's level by 1 after fighting."""
         self.level += 1
 
     def frost_breath(self):
+        """Ice dragon attacks with freezing breath."""
         self.mouth = 'open'
         self.breath = 'freezing'
         print(f'{self.name}\'s mouth is {self.mouth}'
