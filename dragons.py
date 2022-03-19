2e238e041c19b4afbf61336ec94000e04747b176
Orinax <chaunceypelton@gmail.com>
2022-03-19 10:31:20 +0700

    More trying to figure out classes

--- dragons.py
+++ dragons.py
@@ -1,11 +1,12 @@
 class Ice_Dragon:
     """A class used to build Ice Dragons."""
 
-    def __init__(self, name, color, level=1):
+    def __init__(self, name, color, level=1, drg_hp=15):
         """Creates an Ice Dragon at level 1."""
         self.name = name
         self.color = color
         self.level = level
+        self.drg_hp = drg_hp
 
     def fight(self):
         """Increases the dragon's level by 1 after fighting."""
