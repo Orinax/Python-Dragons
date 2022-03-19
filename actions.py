e17fa6e4d864f612532d1296b2a42dde50c5d4ee
Orinax <chaunceypelton@gmail.com>
2022-03-19 23:02:07 +0700

    Understanding more about classes

--- actions.py
+++ actions.py
@@ -1,1 +1,8 @@
+from dragons import Ice_Dragon
 
+
+def attack(target, hp):
+    print(f"{target} is attacked.")
+    hp = hp - 2
+    print(f"{target}'s health is now {hp}")
+    return hp
---
+++ idas_qs.py
@@ -0,0 +1,11 @@
+while True:
+    q1 = input("What's your favorite color?: ")
+
+    if q1 == "red":
+        print("You will be a doctor.")
+    elif q1 == "green":
+        print("You will be an artist.")
+    elif q1 == "yellow":
+        print("You will be a scientist.")
+    elif q1 == "blue":
+        print("You will be a fisherman.")
